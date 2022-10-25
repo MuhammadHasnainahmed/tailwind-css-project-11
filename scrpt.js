01bfba5d8bbc3fd055c5d2e99903036b11e6bb1e
= <=>
2022-10-25 18:17:26 +0500

    all commit

---
+++ scrpt.js
@@ -0,0 +1,8 @@
+function hamburgerClcik () {
+    document.getElementById("menu").style.display  = 'flex';
+    document.getElementById("menu").classList.add('menuAnimation')  ;
+ } 
+function hamburgerClcikOut () {
+    document.getElementById("menu").style.display  = 'none';
+    // alert('k')
+ } 
